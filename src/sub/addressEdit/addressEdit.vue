db078b59c22fd4676197c5948dc7079d139bd602
hm243695czl <1224311695@qq.com>
2022-07-22 16:43:09 +0800

    添加【收货地址】

---
+++ src/sub/addressEdit/addressEdit.vue
@@ -0,0 +1,138 @@
+<template>
+	<view class="address-edit-container">
+		<view class="form w100">
+			<uni-forms ref="form" :rules="state.rules" :modelValue="state.form" labelWidth="80px">
+				<uni-forms-item label="姓名" required name="name">
+					<uni-easyinput v-model="state.form.name" placeholder="请输入姓名" />
+				</uni-forms-item>
+				<uni-forms-item label="手机号码" required name="tel">
+					<uni-easyinput v-model="state.form.tel" placeholder="请输入手机号码" type="number" />
+				</uni-forms-item>
+				<uni-forms-item label="省市区" required name="address" class="text-over" >
+					<uni-data-picker 
+						placeholder="请选择省份、城市、区县"
+						popup-title="请选择省份、城市、区县" 
+						:localdata="state.areaTree" 
+						v-model="state.form.address"
+						:map="state.map"
+						@change="changeAddress" 
+					>
+					</uni-data-picker>
+				</uni-forms-item>
+				<uni-forms-item label="详情地址" required name="addressDetail">
+					<uni-easyinput v-model="state.form.addressDetail" placeholder="请输入详情地址" />
+				</uni-forms-item>
+				<uni-forms-item label="默认地址" required name="isDefault">
+					<uni-data-checkbox v-model="state.form.isDefault"
+					 :localdata="state.typeList" 
+					 selectedColor="#36c1ba"
+					 selectedTextColor="#36c1ba"
+					 />
+				</uni-forms-item>
+			</uni-forms>
+			<view class="btn-box flex-between">
+				<view class="cancel-btn" @click="clickCancel()">取消</view>
+				<view class="confirm-btn" @click="clickConfirm()">保存</view>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script setup lang="ts">
+	import { ref, reactive } from 'vue';
+	import { areaTree } from '@/utils/areaTree';
+	
+	const formRef = ref();
+	const state = reactive({
+		form: {
+			name: '',
+			tel: '',
+			address: '',
+			province: '',
+			city: '',
+			county: '',
+			addressDetail: '',
+			isDefault: 0
+		},
+		rules: {
+			name: {
+				rules: [
+					{ required: true, errorMessage: '姓名不能为空'}
+				]
+			},
+			tel: {
+				rules: [
+					{ required: true, errorMessage: '手机号码不能为空'}
+				]
+			},
+			addressDetail: {
+				rules: [
+					{ required: true, errorMessage: '详细地址不能为空'}
+				]
+			}
+		},
+		typeList: [
+			{ text: '是', value: 1},
+			{ text: '否', value: 0}
+		],
+		areaTree,
+		map: {
+			text: 'value',
+			value: 'value'
+		}
+	});
+	
+	const changeAddress = e => {
+		state.form.province = e.detail.value[0].value;
+		state.form.city = e.detail.value[1].value;
+		state.form.county = e.detail.value[2].value;
+	};
+	
+	const clickCancel = () => {
+		uni.navigateBack();
+	};
+	
+	const clickConfirm = () => {
+		formRef.value.validate(valid => {
+			if (valid === null) {
+				console.log(state.form);
+			}
+		})
+	}
+</script>
+
+<style lang="scss">
+	.address-edit-container{
+		padding: $uni-padding;
+		height: 100%;
+		background-color: $uni-color-white;
+		box-sizing: border-box;
+		.form{
+			margin-top: $uni-padding;
+			button{
+				color: $uni-color-white;
+				background-color: $uni-color-base;
+			}
+			.btn-box{
+				margin-top: $uni-padding;
+				padding: $uni-padding;
+				.cancel-btn{
+					flex: 1;
+					padding: $uni-padding-half;
+					text-align: center;
+					background-color: #f56c6c;
+					color: $uni-color-white;
+					border-radius: 10rpx 0 0 10rpx;
+				}
+				.confirm-btn{
+					flex: 1;
+					padding: $uni-padding-half;
+					text-align: center;
+					background-color: $uni-color-base;
+					color: $uni-color-white;
+					border-radius: 0 10rpx 10rpx 0;
+				}
+			}
+		}
+	}
+</style>
---
+++ src/sub/addressList/addressList.vue
@@ -0,0 +1,33 @@
+<template>
+	<view class="address-list-container">
+		<view class="btn-box">
+			<view class="btn" @click="showEditAddress()">添加收货地址</view>
+		</view>
+	</view>
+</template>
+
+<script setup lang="ts">
+	import { ref } from 'vue';
+	
+	const showEditAddress = () => {
+		uni.navigateTo({
+			url: '/sub/addressEdit/addressEdit'
+		});
+	}
+</script>
+
+<style lang="scss">
+	.address-list-container{
+		.btn-box{
+			margin-top: $uni-padding;
+			padding: $uni-padding;
+			.btn{
+				border-radius: 10rpx;
+				padding: $uni-padding 0;
+				text-align: center;
+				background-color: $uni-color-base;
+				color: $uni-color-white;
+			}
+		}
+	}
+</style>
